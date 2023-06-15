60bd422884604a0d7ff9855d294896c7d91a3d36
Левин Вадим Валерьевич <pr120levin@oat.local>
2023-06-15 16:16:50 +0600

    First commit

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Analis.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+public class Analis extends AppCompatActivity   {
+
+    float x1, x2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analis);
+    }
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch (touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+            x1 = touchEvent.getX();
+
+            break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+
+
+                if(x1 > x2){
+                    Intent i = new Intent(Analis.this, notifation.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
+
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/notifation.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class notifation extends AppCompatActivity {
+float x1,x2,y1,y2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifation);
+    }
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch (touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchEvent.getX();
+                y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+
+                if (x1 < x2){
+                    Intent i = new Intent(notifation.this, Analis.class);
+                    startActivity(i);
+                }else if(x1 > x2){
+                    Intent i = new Intent(notifation.this, Mot.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
+}}
